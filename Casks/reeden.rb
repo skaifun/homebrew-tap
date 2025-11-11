4fd5abe995517921c020fe735699c4c8adea8fe7
skaifun <dev@skai.fun>
2025-11-11 15:15:34 +0800

    Add reeden cask

---
+++ Casks/reeden.rb
@@ -0,0 +1,24 @@
+cask "reeden" do
+  version "1.16.3+332"
+  sha256 "f61af06ca3f02dc227731662cdd8a26357035f603109330cbd91b0da819427de"
+
+  url "https://download.reeden.app/Reeden/#{version}/Reeden-#{version}-macos.dmg"
+  name "Reeden"
+  desc "现代跨平台电子书阅读器"
+  homepage "https://reeden.app/"
+
+  livecheck do
+    url "https://reeden.app/download"
+    regex(%r{href=.*?/Reeden[._-](\d+(?:\.\d+)*\+\d+)-macos\.dmg}i)
+    strategy :page_match
+  end
+
+  app "Reeden.app"
+
+  zap trash: [
+    "~/Library/Application Support/app.reeden",
+    "~/Library/Preferences/app.reeden.plist",
+    "~/Library/Caches/app.reeden",
+    "~/Library/HTTPStorages/app.reeden",
+  ]
+end
